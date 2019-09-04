87064a31a37becd9a321c49a54f8044b32ff3388
Andre Pires <andrecabralpires@gmail.com>
2019-09-04 23:16:34 +0100

    fixes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,9 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
-  after_save :welcome_restaurant
+  after_save :send_welcome_restaurant
+  private
+
+  def send_welcome_restaurant
+    RestaurantMailer.with(user: self).welcome_restaurant.deliver_now
+  end
 end
